79bcc72d73f2071c4db9ce8d40f00c548cfb12e8
dnsng <denise.n.gameiro@sapo.pt>
2015-12-21 17:59:19 +0000

    Test

---
+++ 3. Getting and Cleaning Data/run_analysis.R
@@ -0,0 +1,31 @@
+### READING DATA
+
+# user
+print("Hi! This function requires that you have the dataset (a folder named UCI HAR Database) in your working directory. \n Do you have it?")
+userInput <- (menu(c("Yes. Go on.", "No. Get me out of here.")))
+
+if(userInput == 2){print("Ok. Good ridance")}
+
+if(userInput == 1){
+  
+  # getting data
+  
+  
+  ### DATA CLEANING
+  
+  # merging train and test sets
+  
+  # extracting mean and SD of measurements
+  
+  # descriptive activity names
+  
+  # descriptive labels
+  
+  # print tidy data set
+  
+  
+  
+  
+  
+    
+}
